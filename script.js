6eafad1b8ac374922cf8a693a2d427da9975adc0
Dennis <ssembatyadennis25@gmail.com>
2022-03-29 13:42:06 +0300

    add functionality to the reset button and ensure the game is working properly and ready for play.

--- script.js
+++ script.js
@@ -9,9 +9,19 @@ document.querySelector('.guess').value = 23;
 console.log((document.querySelector('.guess').value = 23));
 */
 
-const secretNumber = Math.trunc(Math.random() * 20 + 1);
+let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+let highScore = 0;
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20 + 1);
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,9 +33,13 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     //when the person guesses the secret number
     document.querySelector('.message').textContent = ' 🎉 Correct Number!';
-
+    document.querySelector('.number').textContent = secretNumber;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
+    }
   } else if (guess > secretNumber) {
     //when the guessed number is too high than the secret number
     if (score > 1) {
